a5dcb66f91964513fe8da9392715780713d24a22
kedaular <bredelem@protonmail.com>
2021-03-06 16:25:17 +0500

    created

---
+++ install.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+os.system("cls")
+os.system("pip install sourcedefender")
+time.sleep(2)
+os.system("pip install pyautogui")
+time.sleep(2)
+os.system("cls")
+time.sleep(1)
+print("Done")
+time.sleep(2)
---
+++ run.py
@@ -0,0 +1,7 @@
+import os
+
+
+run_defend = "python -m sourcedefender"
+cwd = "z9rca2hx7ARk5zEd.pye"
+cmd = "{0} {1}".format(run_defend,cwd)
+os.system(cmd)
